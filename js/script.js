278c4d25373d48cbb441b9db7d8da046e75a5adc
Your NameIrina-S <serysheva.i@yandex.ru>
2021-02-01 00:14:54 +0900

    Закончена адаптация главной. Начата корзина

--- js/script.js
+++ js/script.js
@@ -63,5 +63,75 @@ $(document).ready(function () {
     $("footer.footer").addClass("footer-fixed-bottom");
   }
 
-  $('[data-fancybox]').fancybox();
+  // $('[data-fancybox]').fancybox();
+
+  // ОТКРЫТИЕ МЕНЮ
+  // $('.main-navigation-btn').on('click', function(){
+  //   $('.aside').addClass('opened');
+  //   $('body').addClass('overflow-hid').addClass('dark');
+  //   $('body::after');
+  // })
+
+  const mobileCatalog = $('.aside');
+  const mobileMenu = $('.main-navigation');
+  const cartBtn = $('.header .header-cart');
+
+  const mobileCatalogBtn = $('.mobile-catalog-btn');
+  const mobileMenuBtn = $('.main-navigation-btn');
+  const cartAside = $('.cart');
+
+  function clickDarkBodyHandler(event){
+    const target = $(event.target);
+    if(target.is('body.dark')){
+      $('body.dark').off('click', clickDarkBodyHandler);
+      $('.opened').removeClass('opened');
+      $(document.body).removeClass('dark').removeClass('overflow-hid');
+    }
+  }
+
+  $(mobileCatalogBtn).click(function () {
+    $(mobileCatalog).toggleClass('opened');
+    $(document.body).toggleClass('dark').toggleClass('overflow-hid');
+
+    if($(document.body).hasClass('dark')){
+      $('body.dark').on('click', clickDarkBodyHandler);
+    }
+  });
+
+  $(mobileMenuBtn).click(function () {
+    $(mobileMenu).toggleClass('opened');
+    $(document.body).toggleClass('dark').toggleClass('overflow-hid');
+
+    if($(document.body).hasClass('dark')){
+      $('body.dark').on('click', clickDarkBodyHandler);
+    }
+  });
+
+  $(cartBtn).click(function (e) {
+    e.preventDefault();
+    console.log('open');
+    $(cartAside).toggleClass('opened');
+    $(document.body).toggleClass('dark').addClass('overflow-hid');
+
+    if($(document.body).hasClass('dark')){
+      $('body.dark').on('click', clickDarkBodyHandler);
+    }
+  });
+
+  $('.aside .close-btn').click(function(){
+    $('.aside').removeClass('opened');
+    $(document.body).removeClass('dark').removeClass('overflow-hid');
+  });
+
+  $('.main-navigation .close-btn').click(function(){
+    $('.main-navigation').removeClass('opened');
+    $(document.body).removeClass('dark').removeClass('overflow-hid');
+  });
+
+  $('.cart .cart-header__close-btn').click(function(){
+      console.log('close');
+    $('.cart').removeClass('opened');
+    $(document.body).removeClass('dark').removeClass('overflow-hid');
+  });
+
 });
